062a5b9a4a56e770b537f6338161d5cc2344289c
Violetta GEORGAKAKOU <vgeorgak@e3r3p21.clusters.42paris.fr>
2022-03-02 18:29:27 +0100

    tag creation

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,65 @@
+/*--------- create filter tags arrays ----------*/
+
+let ingredientSearch = [];
 let ingredientArray = [];
 
-const   ingredientTag = () =>{
-    const a = recipes.ingredients;
-    for (i = 0; i < a.length; i++){
-        ingredientArray[i] = a.ingredient;
-        console.log(ingredientArray[i]);
+const ingredientTag = async () => {
+    for (var i = 0; i < recipes.length; i++) {
+        ingredientSearch = recipes[i].ingredients;
+        for (var i2 = 0; i2 < ingredientSearch.length; i2++) {
+            let ingredientProducts = ingredientSearch[i2].ingredient;
+            if (ingredientArray.includes(ingredientProducts))
+                break;
+            else
+                ingredientArray.push(ingredientProducts);
+        }
     }
 }
 
+let applianceArray = [];
+
+const applianceTag = async () => {
+    
+    for (var i = 0; i < recipes.length; i++)
+    {
+        let applianceProducts = recipes[i].appliance;
+        if (!applianceArray.includes(applianceProducts))
+            applianceArray.push(applianceProducts);
+    }
+    //console.log(applianceArray);
+}
+
+let ustensileSearch = [];
+let ustensilArray = [];
+
+const ustensilesTag = async () => {
+
+    for (var i = 0; i < recipes.length; i++) {
+        ustensileSearch = recipes[i].ustensils;
+        for (var i2 = 0; i2 < ustensileSearch.length; i2++) {
+            let ustensileProduct = ustensileSearch[i2];
+            if (!ustensilArray.includes(ustensileProduct))
+                ustensilArray.push(ustensileProduct);
+        }
+    }
+}
+
+/*---- display filter tags ----*/
+let filterIngredientBtn = document.querySelector("#ingredient_btn");
+let filterApplianceBtn = document.querySelector("#appliances_btn");
+let filterUstensilBtn = document.querySelector("#ustensiles_btn");
+
+filterIngredientBtn.onfocus = () => {
+    let 
+}
+
 const init = async () => {
-    console.log(recipes);
+ //   console.log(recipes);
 };
 
 window.onload = () => {
     init();
     ingredientTag();
+    applianceTag();
+    ustensilesTag();
 }
